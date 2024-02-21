ff73238e93891bfd82c4682a21a517cc4768c6a5
Naleo-4 <90037354+Naleo-4@users.noreply.github.com>
2024-02-21 07:08:51 +0700

    Lesson 2

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -1,9 +1,38 @@
+using System;
 using UnityEngine;
 
+public enum HexDirection {
+    NE, E, SE, SW, W, NW
+}
+
+
+public static class HexDirectionExtensions {
+    public static HexDirection Opposite (this HexDirection direction) {
+        return (int)direction < 3 ? (direction + 3) : (direction - 3);
+    }
+    public static HexDirection Previous (this HexDirection direction) {
+        return direction == HexDirection.NE ? HexDirection.NW : (direction - 1);
+    }
+
+    public static HexDirection Next (this HexDirection direction) {
+        return direction == HexDirection.NW ? HexDirection.NE : (direction + 1);
+    }
+}
 public class HexCell : MonoBehaviour
 {
+    [SerializeField]
+    HexCell[] neighbors;
     [SerializeField]
     public HexCoordinates coordinates;
     
     public Color color;
+    public HexCell GetNeighbor (HexDirection direction) {
+        return neighbors[(int)direction];
+    }
+    public void SetNeighbor (HexDirection direction, HexCell cell) {
+        neighbors[(int)direction] = cell;
+        cell.neighbors[(int)direction.Opposite()] = this;
+    }
+    
+    
 }
